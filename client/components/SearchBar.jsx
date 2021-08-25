5b1abcbc497a51e3cf7abfe5c5ce70871f7bcfb3
Nicholas Schillaci <Schillaci.Nicholas@gmail.com>
2021-08-24 17:12:59 -0700

    committing fully functional api request

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -5,19 +5,23 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 const SearchBar = () => {
   const [myOptions, setMyOptions] = useState([]);
 
-  const getDataFromAPI = () => {
+  const getDataFromAPI = (value) => {
+    console.log('value', value);
     console.log('Options Fetched from API');
+   
 
+    //api_key=fdbae0761ba6f8241b3cf31d7a03a2f9
     fetch(
-      'https://api.themoviedb.org/3/movie/550?api_key=fdbae0761ba6f8241b3cf31d7a03a2f9'
+      `https://api.themoviedb.org/3/search/movie?api_key=fdbae0761ba6f8241b3cf31d7a03a2f9&language=en-US&page=1&include_adult=false&query=${value}`
     )
       .then((response) => {
         return response.json();
       })
       .then((res) => {
-        console.log(res.data);
-        for (let i = 0; i < res.data.length; i++) {
-          myOptions.push(res.data[i].employee_name);
+        console.log(res);
+        for (let i = 0; i < res.results.length; i++) {
+          if(!myOptions.includes(res.results[i].title)) 
+          myOptions.push(res.results[i].title);
         }
         setMyOptions(myOptions);
       });
@@ -30,12 +34,13 @@ const SearchBar = () => {
         style={{ width: 500 }}
         freeSolo
         autoComplete
+        selectOnFocus
         autoHighlight
         options={myOptions}
         renderInput={(params) => (
           <TextField
             {...params}
-            onChange={getDataFromAPI}
+            onChange={(event) => getDataFromAPI(event.target.value)}
             variant="outlined"
             label="Search Box"
           />
